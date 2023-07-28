b777fca107b6eb107b1909d4df859d75599eb25e
alialaa <hello@alialaa.com>
2023-07-28 22:34:41 +0300

    flagsmith

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link";
+import flagsmith from "../utils/flagsmith";
 
-export default function Home() {
+export default async function Home() {
+  const flags = await flagsmith.getEnvironmentFlags();
   return (
     <main className="main">
       <div>
         <h2>Hello World</h2>
       </div>
       <Link href="/about">About</Link>
+      {flags.isFeatureEnabled("search") && <input />}
     </main>
   );
 }
