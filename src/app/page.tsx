922eae2e47aa2a1a5903517c3af182de3fff94c4
alialaa <hello@alialaa.com>
2023-07-29 17:35:46 +0300

    dynamic page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import flagsmith from "../utils/flagsmith";
 
+export const revalidate = 0
+
 export default async function Home() {
   const flags = await flagsmith.getEnvironmentFlags();
   return (
