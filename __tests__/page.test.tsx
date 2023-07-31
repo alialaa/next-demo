f0f33ecacac1252dcb010bac64ec7ef5cc72bb9c
alialaa <hello@alialaa.com>
2023-07-31 17:12:36 +0300

    ci wf

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -2,9 +2,9 @@ import { render, screen } from '@testing-library/react'
 import Home from '../src/app/page'
 import '@testing-library/jest-dom'
  
-describe('Home', () => {
-  it('renders a heading', () => {
-    render(<Home />)
+describe('Home',  () => {
+  it('renders a heading', async() => {
+    render(await  Home())
  
     const docH = screen.getByRole('heading', {
       name: "Hello World",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
-import flagsmith from "../utils/flagsmith";
+// import flagsmith from "../utils/flagsmith";
 
 export const revalidate = 0
 
 export default async function Home() {
-  const flags = await flagsmith.getEnvironmentFlags();
+  // const flags = await flagsmith.getEnvironmentFlags();
   return (
     <main className="main">
       <div>
@@ -12,7 +12,7 @@ export default async function Home() {
         <p>{process.env.FLAGSMITH_KEY || 'no key'}</p>
       </div>
       <Link href="/about">About</Link>
-      {flags.isFeatureEnabled("search") && <input />}
+      {/* {flags.isFeatureEnabled("search") && <input />} */}
     </main>
   );
 }
