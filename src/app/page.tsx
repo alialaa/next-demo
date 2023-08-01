b66d2c0c7a2b921bf065b62312ca3063e3cdd653
alialaa <hello@alialaa.com>
2023-08-01 11:24:32 +0300

    flagsmith mock

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 import Link from "next/link";
-// import flagsmith from "../utils/flagsmith";
+import flagsmith from "../utils/flagsmith";
 
 export const revalidate = 0;
 
 export default async function Home() {
-  // const flags = await flagsmith.getEnvironmentFlags();
+  const flags = await flagsmith.getEnvironmentFlags();
   return (
     <main className="main">
       <div>
         <h2>Hello World</h2>
       </div>
       <Link href="/about">About</Link>
-      {/* {flags.isFeatureEnabled("search") && <input />} */}
+      {flags.isFeatureEnabled("search") && <input />}
     </main>
   );
 }
