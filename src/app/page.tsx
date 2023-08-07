e1b3b73f0f7ed073926b064b40144af3e9fa7a01
alialaa <hello@alialaa.com>
2023-08-07 12:10:46 +0300

    test deploy to main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
     <main className="main">
       <div>
         <h2>Hello World</h2>
-        <p>hello?</p>
+        <p>new life, new hope!!</p>
       </div>
       <Link href="/about">About</Link>
       {flags.isFeatureEnabled("search") && <input />}
